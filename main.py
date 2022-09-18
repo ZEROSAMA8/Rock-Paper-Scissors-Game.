d12f78a374cd8c8b48c09d89762f44aff7c604f6
ZEROSAMA8 <113395068+ZEROSAMA8@users.noreply.github.com>
2022-09-18 19:35:19 +0000

    change

--- main.py
+++ main.py
@@ -30,20 +30,23 @@ import random
 print("Wlecom to rock paper scissoers game :))))))")
 game =[rock,paper,scissors] 
 ask = int(input("What do you choose? Type 0 for Rock, 1 for Paper and 2 for Scissors.\n "))
-print(game[ask])
-
-compu_cho = random.randint(0,2)
-print("Computer chose:\n")
-print(game[compu_cho])
 if ask >=3:
   print("You typed an invalis number, You lost!")
-elif ask ==0 and compu_cho ==2:
+else:
+ print(game[ask])
+
+ compu_cho = random.randint(0,2)
+ print("Computer chose:\n")
+ print(game[compu_cho])
+# if ask >=3:
+#   print("You typed an invalis number, You lost!")
+ if ask ==0 and compu_cho ==2:
   print("Congratulations ::)))))), You won this game.\n Would you like to play with me again? I will win this time ;")
-elif compu_cho==0 and ask==2:
+ elif compu_cho==0 and ask==2:
   print("You lost this game:(( Would you like to play again?")
-elif compu_cho > ask:
+ elif compu_cho > ask:
   print("You lost this game:(( Would you like to play again?")
-elif ask > compu_cho:
+ elif ask > compu_cho:
   print("Congratulations ::)))))), You won this game.\n Would you like to play with me again? I will win this time ;")
-elif compu_cho == ask :
+ elif compu_cho == ask :
   print("You Draw with Computer.")
